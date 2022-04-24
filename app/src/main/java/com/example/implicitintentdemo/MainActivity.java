4882b47ef3daa10c45b8cc4edec3e6a551095185
samarth-eng <80321834+samarth-eng@users.noreply.github.com>
2022-04-24 16:32:56 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/implicitintentdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.implicitintentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnShare = findViewById(R.id.btnShare);
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "Hello Friends I am Akshay");
+                startActivity(intent.createChooser(intent, "Share"));
+            }
+        });
+    }
+}
